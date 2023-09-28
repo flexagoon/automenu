516ee06791d7c7b7c7ff865f1f1d7806d43c1255
flexagoon <code@fxgn.dev>
2023-09-28 11:36:42 +0000

    refactor: move menu generation into a module

---
+++ MenuMaker.jl
@@ -0,0 +1,143 @@
+module MenuMaker
+
+using PDFIO
+using DataFrames
+using JuMP
+using HiGHS
+
+export makemenu
+
+function makemenu(filename::AbstractString)
+    file = pdDocOpen(filename)
+
+    pagecount = pdDocGetPageCount(file)
+
+    menu = []
+    for i in 1:pagecount
+        page = pdDocGetPage(file, i)
+
+        if i != 5
+            breakfast, lunch = parsepage(page)
+            push!(menu, (breakfast=breakfast, lunch=lunch))
+        else
+            lunch = parsepage(page, false)
+            push!(menu, (breakfast=nothing, lunch=lunch))
+        end
+    end
+
+    return menu
+end
+
+function parsepage(page::PDPage, includebreakfast=true)
+    text = sprint(pdPageExtractText, page)
+
+    text = replace(text, r"[\n ]{2,}" => ";", "," => ".")
+
+    lunch = match(r"ОБЕД;(.*);ПОЛДНИК", text)[1]
+    lunch = parsefoods(lunch)
+
+    if includebreakfast
+        breakfast = match(r"ЗАВТРАК;(.*);ОБЕД", text)[1]
+        breakfast = parsefoods(breakfast)
+
+        calculateplan!(breakfast, lunch)
+
+        filter!(row -> row.eat, breakfast)
+        filter!(row -> row.eat, lunch)
+
+        return breakfast, lunch
+    else
+        calculateplan!(lunch)
+
+        filter!(row -> row.eat, lunch)
+
+        return lunch
+    end
+end
+
+const foodpattern = r"([^\d;]+);([\d.]+);([\d.]+);([\d.]+);([\d.]+);\d+ руб.;(\d+) гр\.;|([\d.]+);([\d.]+);([\d.]+);([\d.]+);([^\d;]+);\d+ руб.;(\d+) гр\.;"
+const blacklist = ["Сарделька из мяса птицы", "Сыр", "Запечённая индейка", "Сосиска из мяса птицы"]
+function parsefoods(menu)
+    matches = eachmatch(foodpattern, menu)
+
+    foods = DataFrame()
+    for match in matches
+        match = match.captures
+        if isnothing(match[7])
+            match = map(x -> something(tryparse(Float64, x), x), match[1:6])
+
+            size = match[6] / 100
+            if !(match[1] in blacklist)
+                push!(foods, (
+                    name=match[1],
+                    calories=match[2] * size,
+                    protein=match[3] * size,
+                    fat=match[4] * size,
+                    carbs=match[5] * size
+                ))
+            end
+        else
+            match = map(x -> something(tryparse(Float64, x), x), match[7:12])
+
+            size = match[6] / 100
+            if !(match[5] in blacklist)
+                push!(foods, (
+                    name=match[5],
+                    calories=match[1] * size,
+                    protein=match[2] * size,
+                    fat=match[3] * size,
+                    carbs=match[4] * size
+                ))
+            end
+        end
+    end
+
+    return foods
+end
+
+function calculateplan!(breakfast, lunch)
+    brange = 1:size(breakfast, 1)
+    lrange = 1:size(lunch, 1)
+
+    model = Model(HiGHS.Optimizer)
+    set_silent(model)
+
+    @variable(model, x[brange], Bin)
+    @variable(model, y[lrange], Bin)
+
+    bcalories = sum(breakfast[i, :calories] * x[i] for i in brange)
+    lcalories = sum(lunch[i, :calories] * y[i] for i in lrange)
+    @constraint(model, bcalories >= 700)
+    @constraint(model, lcalories >= 700)
+    @constraint(model, bcalories + lcalories <= 1800)
+
+    bprotein = sum(breakfast[i, :protein] * x[i] for i in brange)
+    lprotein = sum(lunch[i, :protein] * y[i] for i in lrange)
+    @objective(model, Max, bprotein + lprotein)
+
+    optimize!(model)
+
+    breakfast[!, "eat"] = [convert(Bool, value(x[i])) for i in brange]
+    lunch[!, "eat"] = [convert(Bool, value(y[i])) for i in lrange]
+
+    return nothing
+end
+
+function calculateplan!(lunch)
+    range = 1:size(lunch, 1)
+
+    model = Model(HiGHS.Optimizer)
+    set_silent(model)
+
+    @variable(model, x[range], Bin)
+    @constraint(model, 700 <= sum(lunch[i, :calories] * x[i] for i in range) <= 900)
+    @objective(model, Max, sum(lunch[i, :protein] * x[i] for i in range))
+
+    optimize!(model)
+
+    lunch[!, "eat"] = [convert(Bool, value(x[i])) for i in range]
+
+    return nothing
+end
+
+end
