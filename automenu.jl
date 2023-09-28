1b5b801c14d3cae65cc899fcd01582ef46b834eb
flexagoon <code@fxgn.dev>
2023-09-28 06:57:38 +0000

    feat: parse foods into dataframes

--- automenu.jl
+++ automenu.jl
@@ -1,4 +1,40 @@
 using PDFIO
+using DataFrames
+
+const foodpattern = r"([^\d;]+);([\d.]+);([\d.]+);([\d.]+);([\d.]+);\d+ руб.;(\d+) гр\.;|([\d.]+);([\d.]+);([\d.]+);([\d.]+);([^\d;]+);\d+ руб.;(\d+) гр\.;"
+function parsefoods(menu)
+    matches = eachmatch(foodpattern, menu)
+
+    foods = DataFrame()
+    for match in matches
+        match = match.captures
+        if isnothing(match[7])
+            @show match = map(x -> something(tryparse(Float64, x), x), match[1:6])
+
+            size = match[6] / 100
+            push!(foods, (
+                name=match[1],
+                calories=match[2] * size,
+                protein=match[3] * size,
+                fat=match[4] * size,
+                carbs=match[5] * size
+            ))
+        else
+            match = map(x -> something(tryparse(Float64, x), x), match[7:12])
+
+            size = match[6] / 100
+            push!(foods, (
+                name=match[5],
+                calories=match[1] * size,
+                protein=match[2] * size,
+                fat=match[3] * size,
+                carbs=match[4] * size
+            ))
+        end
+    end
+
+    return foods
+end
 
 file = pdDocOpen("menu.pdf")
 
@@ -7,18 +43,10 @@ pages = pdDocGetPageCount(file)
 page = pdDocGetPage(file, 1)
 text = sprint(pdPageExtractText, page)
 
-text = replace(text, r"[\n ]{2,}" => ";")
+text = replace(text, r"[\n ]{2,}" => ";", "," => ".")
 
 breakfast = match(r"ЗАВТРАК;(.*);ОБЕД", text)[1]
 lunch = match(r"ОБЕД;(.*);ПОЛДНИК", text)[1]
 
-const meals = r"([^\d;]+);([\d,]+);([\d,]+);([\d,]+);([\d,]+);\d+ руб.;(\d+) гр\.;|([\d,]+);([\d,]+);([\d,]+);([\d,]+);([^\d;]+);\d+ руб.;(\d+) гр\.;"
-
-breakfastmeals = eachmatch(meals, breakfast)
-for meal in breakfastmeals
-    if isnothing(meal[7])
-        println("$(meal[1]): $(meal[2]) ккал, $(meal[3]) Б, $(meal[4]) Ж, $(meal[5]) У, $(meal[6]) гр.")
-    else
-        println("$(meal[11]): $(meal[7]) ккал, $(meal[8]) Б, $(meal[9]) Ж, $(meal[10]) У, $(meal[12]) гр.")
-    end
-end
+breakfast = parsefoods(breakfast)
+lunch = parsefoods(lunch)
