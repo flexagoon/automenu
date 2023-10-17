9539973b6b3baea7e09285b4b4d2fc836b0ab68d
flexagoon <code@fxgn.dev>
2023-10-17 12:30:05 +0300

    fix: update menu printing to new data structure

--- src/AutoMenu.jl
+++ src/AutoMenu.jl
@@ -4,10 +4,14 @@ include("GoogleDrive.jl")
 using .MenuMaker
 using .GoogleDrive
 
-total_nutrition(::Nothing, lunch) = sum(lunch.calories), sum(lunch.protein)
-total_nutrition(breakfast, lunch) = sum(breakfast.calories) + sum(lunch.calories), sum(breakfast.protein) + sum(lunch.protein)
+total_nutrition(::Nothing, lunch) = sum(lunch -> lunch.calories, lunch), sum(lunch -> lunch.protein, lunch)
+total_nutrition(breakfast, lunch) = sum(b -> b.calories, breakfast) + sum(lunch -> lunch.calories, lunch), sum(b -> b.protein, breakfast) + sum(lunch -> lunch.protein, lunch)
+
+println("Downloading menu...")
 
 GoogleDrive.download("1d9p3y0gJz6YDLn2kjkzjFIGvx2NX8JNX", "menu.pdf")
+
+println("Generating meal plan...")
 menu = makemenu("menu.pdf")
 
 for day in menu
@@ -20,14 +24,14 @@ for day in menu
     if !isnothing(day.breakfast)
         println()
         println("ЗАВТРАК")
-        for food in eachrow(day.breakfast)
+        for food in day.breakfast
             println(food.name)
         end
     end
 
     println()
     println("ОБЕД")
-    for food in eachrow(day.lunch)
+    for food in day.lunch
         println(food.name)
     end
 end
